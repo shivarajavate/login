f4ecb7d94e7c1ea96ec682814793ff68ef36d165
shivaraj_avate <shivaraj_avate@resourcepro.in>
2023-07-07 11:56:10 +0530

    feat: added react-datepicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import DragDropUpload from './DragDropUpload';
+import './App.css';
 
 const App: React.FC = () => {
   const handleFileUpload = (file: File) => {
@@ -7,8 +10,35 @@ const App: React.FC = () => {
     // Perform further actions with the file
   };
 
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+  const handleDateChange = (date: Date | null) => {
+    setSelectedDate(date);
+  };
+
   return (
-    <div>
+    <div className='app'>
+      <pre>
+        <div className='code-snippet'>
+          <label htmlFor="code" className='copy-code'>copy code</label>
+          <code id="code">
+            <div>
+              npm install react-datepicker @types/react-datepicker
+            </div>
+            <div>
+              import React from 'react';
+            </div>
+            <div>
+              import DatePicker from 'react-datepicker';
+            </div>
+            <div>
+              import 'react-datepicker/dist/react-datepicker.css';
+            </div>
+          </code>
+        </div>
+      </pre>
+      <DatePicker
+        selected={selectedDate} onChange={handleDateChange} />
       <h1>Drag and Drop Upload</h1>
       <DragDropUpload onFileUpload={handleFileUpload} />
     </div>
